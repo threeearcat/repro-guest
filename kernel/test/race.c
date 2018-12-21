d80c7c7c4d0a0fca1f149772c6715da9b73f5236
Dae R. Jeong <threeearcat@gmail.com>
2018-12-21 10:56:09 +0900

    kernel/test: Adding syscalls for testing

---
+++ kernel/test/race.c
@@ -0,0 +1,34 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/sched.h>
+#include <linux/syscalls.h>
+#include <linux/slab.h>
+
+char *buf;
+
+#pragma GCC push_options
+#pragma GCC optimize ("O0")
+
+#define STR "Writing to buffer"
+SYSCALL_DEFINE0(race1) {
+
+	buf = kmalloc(1024, GFP_KERNEL);
+	if (buf) {
+		// race window
+		memcpy(buf, STR, strlen(STR));
+		buf[strlen(STR)] = 0;
+		printk(KERN_INFO "%s\n", buf);
+		kfree(buf);
+	}
+
+	return 0;
+}
+
+SYSCALL_DEFINE0(race2) {
+
+	buf = NULL;
+	return 0;
+}
+
+
+#pragma GCC pop_options
