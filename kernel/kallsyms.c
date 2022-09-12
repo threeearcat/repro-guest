0a97f458571967a3b953a9c2144734b38f51c4d9
Dae R. Jeong <threeearcat@gmail.com>
2022-09-12 10:57:03 +0000

    Revert "kallsyms: unexport kallsyms_lookup_name() and kallsyms_on_each_symbol()"
    This reverts commit 0bd476e6c67190b5eb7b6e105c8db8ff61103281.

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -208,6 +208,7 @@ unsigned long kallsyms_lookup_name(const char *name)
 	}
 	return module_kallsyms_lookup_name(name);
 }
+EXPORT_SYMBOL_GPL(kallsyms_lookup_name);
 
 /*
  * Iterate over all symbols in vmlinux.  For symbols from modules use
@@ -231,6 +232,7 @@ int kallsyms_on_each_symbol(int (*fn)(void *, const char *, struct module *,
 	}
 	return 0;
 }
+EXPORT_SYMBOL_GPL(kallsyms_on_each_symbol);
 
 static unsigned long get_symbol_pos(unsigned long addr,
 				    unsigned long *symbolsize,
